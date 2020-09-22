c67c54466b3d309c5e99441f77ecb2a0c2b46d86
luoxiao5566 <39615443+luoxiao5566@users.noreply.github.com>
2020-09-23 01:08:17 +0800

    Add files via upload

---
+++ src/Introduce.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Introduce.css';
+
+class Introduce extends React.Component {
+  render() {
+    return <div>     
+        <h1 class="h1One">
+             HELLO,
+        </h1>
+        <h1 className="h1Two">
+             MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV
+        </h1>
+      </div>
+    
+    }
+}
+
+export default Introduce;
